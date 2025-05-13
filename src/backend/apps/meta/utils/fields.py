24fd4765503ff4cfa46f35ead9f6ca52f950b920
JetDrag <taptube@gmail.com>
2025-05-13 17:03:15 +0800

    feat: 规则审计：风险发现规则支持字段下钻  --story=123860426

--- src/backend/apps/meta/utils/fields.py
+++ src/backend/apps/meta/utils/fields.py
@@ -16,7 +16,7 @@ We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
 
-from typing import Union
+from typing import Optional, TypedDict, Union
 
 from django.conf import settings
 from django.utils.translation import gettext_lazy
@@ -67,6 +67,19 @@ PYTHON_TO_ES = {
     dict: FIELD_TYPE_OBJECT,
     float: FIELD_TYPE_DOUBLE,
 }
+
+
+class SubKey(TypedDict):
+    field_name: str
+    field_type: str
+    field_alias: str
+    property: Optional['FieldProperty']
+
+
+class FieldProperty(TypedDict):
+    dynamic_content: bool
+    sub_keys: list[SubKey]
+
 
 EVENT_ID = Field(
     field_name="event_id",
@@ -292,6 +305,7 @@ INSTANCE_DATA = Field(
     option={"meta_field_type": FIELD_TYPE_TEXT},
     is_required=False,
     priority_index=80,
+    property=FieldProperty(dynamic_content=True, sub_keys=[]),
 )
 
 INSTANCE_ORIGIN_DATA = Field(
@@ -304,6 +318,7 @@ INSTANCE_ORIGIN_DATA = Field(
     option={"meta_field_type": FIELD_TYPE_TEXT},
     is_required=False,
     priority_index=79,
+    property=FieldProperty(dynamic_content=True, sub_keys=[]),
 )
 
 RESULT_CODE = Field(
@@ -338,6 +353,7 @@ EXTEND_DATA = Field(
     option={"meta_field_type": FIELD_TYPE_TEXT},
     is_required=False,
     priority_index=76,
+    property=FieldProperty(dynamic_content=True, sub_keys=[]),
 )
 
 SNAPSHOT_USER_INFO = Field(
@@ -349,6 +365,31 @@ SNAPSHOT_USER_INFO = Field(
     is_required=False,
     is_display=False,
     is_json=True,
+    property=FieldProperty(
+        dynamic_content=False,
+        sub_keys=[
+            SubKey(field_name="department_full_name", field_type=FIELD_TYPE_STRING, field_alias="主岗全称", property=None),
+            SubKey(field_name="department_id", field_type=FIELD_TYPE_INT, field_alias="主岗ID", property=None),
+            SubKey(field_name="department_name", field_type=FIELD_TYPE_STRING, field_alias="主岗名称", property=None),
+            SubKey(field_name="department_path", field_type=FIELD_TYPE_STRING, field_alias="主岗路径ID", property=None),
+            SubKey(field_name="dimission_date", field_type=FIELD_TYPE_STRING, field_alias="离职时间", property=None),
+            SubKey(field_name="display_name", field_type=FIELD_TYPE_STRING, field_alias="展示名称", property=None),
+            SubKey(field_name="enabled", field_type=FIELD_TYPE_INT, field_alias="帐户是否启用", property=None),
+            SubKey(field_name="gender", field_type=FIELD_TYPE_STRING, field_alias="性别", property=None),
+            SubKey(field_name="id", field_type=FIELD_TYPE_INT, field_alias="ID", property=None),
+            SubKey(field_name="leader_username", field_type=FIELD_TYPE_STRING, field_alias="直属上级", property=None),
+            SubKey(field_name="level_name", field_type=FIELD_TYPE_STRING, field_alias="技术职级", property=None),
+            SubKey(field_name="manager_level", field_type=FIELD_TYPE_INT, field_alias="管理职级", property=None),
+            SubKey(field_name="manager_unit_name", field_type=FIELD_TYPE_INT, field_alias="签约主体", property=None),
+            SubKey(field_name="move_date", field_type=FIELD_TYPE_STRING, field_alias="异动时间", property=None),
+            SubKey(field_name="move_id", field_type=FIELD_TYPE_STRING, field_alias="异动编号", property=None),
+            SubKey(field_name="move_type_id", field_type=FIELD_TYPE_INT, field_alias="异动类型", property=None),
+            SubKey(field_name="phone", field_type=FIELD_TYPE_STRING, field_alias="电话", property=None),
+            SubKey(field_name="staff_status", field_type=FIELD_TYPE_INT, field_alias="员工状态", property=None),
+            SubKey(field_name="staff_type", field_type=FIELD_TYPE_INT, field_alias="员工类型", property=None),
+            SubKey(field_name="username", field_type=FIELD_TYPE_STRING, field_alias="用户名", property=None),
+        ],
+    ),
 )
 
 SNAPSHOT_RESOURCE_TYPE_INFO = Field(
@@ -360,6 +401,24 @@ SNAPSHOT_RESOURCE_TYPE_INFO = Field(
     is_required=False,
     is_display=False,
     is_json=True,
+    property=FieldProperty(
+        dynamic_content=False,
+        sub_keys=[
+            SubKey(field_name="created_at", field_type=FIELD_TYPE_LONG, field_alias="创建时间", property=None),
+            SubKey(field_name="created_by", field_type=FIELD_TYPE_STRING, field_alias="创建者", property=None),
+            SubKey(field_name="description", field_type=FIELD_TYPE_STRING, field_alias="描述信息", property=None),
+            SubKey(field_name="id", field_type=FIELD_TYPE_INT, field_alias="资源类型ID", property=None),
+            SubKey(field_name="name", field_type=FIELD_TYPE_STRING, field_alias="资源名称", property=None),
+            SubKey(field_name="name_en", field_type=FIELD_TYPE_STRING, field_alias="英文资源名称", property=None),
+            SubKey(field_name="resource_type_id", field_type=FIELD_TYPE_STRING, field_alias="资源类型ID", property=None),
+            SubKey(field_name="sensitivity", field_type=FIELD_TYPE_INT, field_alias="敏感等级", property=None),
+            SubKey(field_name="system_id", field_type=FIELD_TYPE_STRING, field_alias="系统ID", property=None),
+            SubKey(field_name="type", field_type=FIELD_TYPE_STRING, field_alias="资源类型", property=None),
+            SubKey(field_name="updated_at", field_type=FIELD_TYPE_LONG, field_alias="更新时间", property=None),
+            SubKey(field_name="updated_by", field_type=FIELD_TYPE_STRING, field_alias="更新者", property=None),
+            SubKey(field_name="version", field_type=FIELD_TYPE_INT, field_alias="版本", property=None),
+        ],
+    ),
 )
 
 SNAPSHOT_ACTION_INFO = Field(
@@ -372,6 +431,25 @@ SNAPSHOT_ACTION_INFO = Field(
     is_display=False,
     is_json=True,
     priority_index=USERNAME.priority_index,
+    property=FieldProperty(
+        dynamic_content=False,
+        sub_keys=[
+            SubKey(field_name="action_id", field_type=FIELD_TYPE_STRING, field_alias="操作ID", property=None),
+            SubKey(field_name="created_at", field_type=FIELD_TYPE_LONG, field_alias="创建时间", property=None),
+            SubKey(field_name="created_by", field_type=FIELD_TYPE_STRING, field_alias="创建者", property=None),
+            SubKey(field_name="description", field_type=FIELD_TYPE_STRING, field_alias="描述信息", property=None),
+            SubKey(field_name="id", field_type=FIELD_TYPE_INT, field_alias="ID", property=None),
+            SubKey(field_name="name", field_type=FIELD_TYPE_STRING, field_alias="操作名称", property=None),
+            SubKey(field_name="name_en", field_type=FIELD_TYPE_STRING, field_alias="操作英文名称", property=None),
+            SubKey(field_name="resource_type_ids", field_type=FIELD_TYPE_STRING, field_alias="资源类型ID", property=None),
+            SubKey(field_name="sensitivity", field_type=FIELD_TYPE_INT, field_alias="敏感等级", property=None),
+            SubKey(field_name="system_id", field_type=FIELD_TYPE_STRING, field_alias="系统ID", property=None),
+            SubKey(field_name="type", field_type=FIELD_TYPE_STRING, field_alias="操作类型", property=None),
+            SubKey(field_name="updated_at", field_type=FIELD_TYPE_LONG, field_alias="更新时间", property=None),
+            SubKey(field_name="updated_by", field_type=FIELD_TYPE_STRING, field_alias="更新者", property=None),
+            SubKey(field_name="version", field_type=FIELD_TYPE_INT, field_alias="版本", property=None),
+        ],
+    ),
 )
 
 SNAPSHOT_INSTANCE_NAME = Field(
@@ -397,6 +475,7 @@ SNAPSHOT_INSTANCE_DATA = Field(
     is_required=False,
     is_display=False,
     is_json=True,
+    property={"dynamic_content": True, "sub_keys": []},
 )
 
 SYSTEM_ID = Field(
@@ -535,6 +614,7 @@ EXT_FIELD_CONFIG = Field(
     is_json=True,
     is_dimension=False,
     is_display=False,
+    property=FieldProperty(dynamic_content=True, sub_keys=[]),
 )
 
 FLOW_MD5 = Field(
